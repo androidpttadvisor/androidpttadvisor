b2cbec74b21c98066ae104a7f6a4232c426afdf7
paul <paul@10.253.99.231>
2013-01-17 17:43:52 -0500

    added PTThistoryItem and started history functionality

---
+++ AndroidPTTAdvisor/src/gov/cdc/PTTHistoryItem.java
@@ -0,0 +1,18 @@
+package gov.cdc;
+
+public class PTTHistoryItem {
+	
+	PTTNode node;
+	PTTAnswer answerChosen;
+	
+	
+	
+	public PTTHistoryItem(PTTNode node, PTTAnswer answerChosen) {
+		super();
+		this.node = node;
+		this.answerChosen = answerChosen;
+	}
+	
+	
+
+}
