f44cfc9f40cf8d7b3ca509daf81e366c4770a0ea
all3vil <all3vil@gmail.com>
2013-01-17 17:45:17 -0500

    Oops

---
+++ AndroidPTTAdvisor/src/gov/cdc/Eula.java
@@ -0,0 +1,78 @@
+package gov.cdc;
+
+/**
+ * This code is based off the example given here:
+ * http://blog.donnfelker.com/2011/02/17/android-a-simple-eula-for-your-android-apps/
+ */
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+
+public class Eula {
+	private Activity mActivity;
+	
+	public Eula(Activity context) {
+		mActivity = context;
+	}
+	
+	private String EULA_PREFIX = "eula_";
+	
+	private PackageInfo getPackageInfo() {
+        PackageInfo pi = null;
+        try {
+             pi = mActivity.getPackageManager().getPackageInfo(mActivity.getPackageName(), PackageManager.GET_ACTIVITIES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return pi;
+    }
+	
+    public void show() {
+        PackageInfo versionInfo = getPackageInfo();
+
+        // the eulaKey changes every time you increment the version number in the AndroidManifest.xml
+final String eulaKey = EULA_PREFIX + versionInfo.versionCode;
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mActivity);
+        boolean hasBeenShown = prefs.getBoolean(eulaKey, false);
+        if(hasBeenShown == false){
+
+         // Show the Eula
+            String title = mActivity.getString(R.string.app_name) + " v" + versionInfo.versionName;
+            
+            //Includes the updates as well so users know what changed.
+            String message = mActivity.getString(R.string.updates) + "\n\n" + mActivity.getString(R.string.eula);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(mActivity)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Mark this version as read.
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putBoolean(eulaKey, true);
+                            editor.commit();
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
+
+@Override
+public void onClick(DialogInterface dialog, int which) {
+// Close the activity as they have declined the EULA
+mActivity.finish();
+}
+                    
+                    });
+            builder.create().show();
+        }
+    }
+	
+}
