8b2c66d24a0b65770bd89677a74841cf20982506
paul <paul@10.0.1.15>
2013-01-21 15:46:42 -0500

    Hid the title bar on the History View window.

--- AndroidPTTAdvisor/src/gov/cdc/HistoryView.java
+++ AndroidPTTAdvisor/src/gov/cdc/HistoryView.java
@@ -3,12 +3,18 @@ package gov.cdc;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class HistoryView extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+     // Make the application Full-Screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        
         setContentView(R.layout.activity_history_view);
     }
 
